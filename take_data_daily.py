f3baec58d1bb7a340d3de8d0d4adbdfa605f7062
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-03-17 08:47:59 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -188,18 +188,18 @@ for name, frame,framew in zip(names,framelist,framelistw):
             try:     
                 if len(frame)>30 and len(framew)>30 and framew['Decision EMA50'].iloc[-1]=='Sell' \
                 and frame['ADX'].iloc[-1]>=adx_value and (frame['MACD_diff'].iloc[-1]<0 or frame['Trend MACD'].iloc[-1]=='Strong')  \
-                and (framew['MACD_diff'].iloc[-1]<0 or framew['Trend MACD'].iloc[-1]=='Strong') and frame['Decision EMA50_cross'].iloc[-1]=='Sell' \   
-                and framew['sup'].iloc[-1]=='-1':    
+                and (framew['MACD_diff'].iloc[-1]<0 or framew['Trend MACD'].iloc[-1]=='Strong') and frame['Decision EMA50_cross'].iloc[-1]=='Sell'\
+                and framew['sup'].iloc[-1]=='-1':
                     sira +=1
                     st.write(str(sira)+" Selling EMA50 for "+ name)
                     st.write(frame.tail(2))
             except Exception as e:
                 st.write(name,e)
-        elif option1 == 'Buy'and option2 == 'MACD':  
-            try:   
+        elif option1 == 'Buy'and option2 == 'MACD':
+            try:
                 if  len(frame)>30 and len(framew)>30 and frame['Decision MACD'].iloc[-1]=='Buy'  \
                 and framew['Decision EMA50'].iloc[-1]=='Buy' and frame['ADX'].iloc[-1]>=adx_value\
-                and (framew['MACD_diff'].iloc[-1]>0 or framew['Trend MACD'].iloc[-1]=='Strong') and framew['sup'].iloc[-1]=='1':    
+                and (framew['MACD_diff'].iloc[-1]>0 or framew['Trend MACD'].iloc[-1]=='Strong') and framew['sup'].iloc[-1]=='1':
                     sira +=1
                     st.write(str(sira)+" Buying Signal MACD/EMA200 for "+ name)
                     st.write(frame.tail(2))
