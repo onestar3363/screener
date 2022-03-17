5c451a40d6a1c9ab782915332b842439c2d250c7
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-03-17 08:41:32 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -176,7 +176,7 @@ for name, frame,framew in zip(names,framelist,framelistw):
             try:
                 if len(frame)>30 and len(framew)>30 and framew['Decision EMA50'].iloc[-1]=='Buy' \
                 and frame['ADX'].iloc[-1]>=adx_value and (frame['MACD_diff'].iloc[-1]>0 or frame['Trend MACD'].iloc[-1]=='Strong')  \
-                and (framew['MACD_diff'].iloc[-1]>0 or framew['Trend MACD'].iloc[-1]=='Strong') and frame['Decision EMA50_cross'].iloc[-1]=='Buy'\ 
+                and (framew['MACD_diff'].iloc[-1]>0 or framew['Trend MACD'].iloc[-1]=='Strong') and frame['Decision EMA50_cross'].iloc[-1]=='Buy' \ 
                 and framew['sup'].iloc[-1]=='1': 
                     sira +=1
                     st.write(str(sira)+" Buying EMA50 for "+ name)
