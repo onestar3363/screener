d249d6246e8a53a5dcf04389116c6e6244867698
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-05-06 02:53:48 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -211,7 +211,8 @@ def get_figures(frame):
     fig = go.Figure()
     fig = plotly.subplots.make_subplots(rows=3, cols=1, shared_xaxes=True,
     vertical_spacing=0.01, row_heights=[0.5,0.2,0.2])
-    fig.add_trace(go.Candlestick(x=frame['Date'].tail(50), open=frame['Open'].tail(50), high=frame['High'].tail(50), low=frame['Low'].tail(50), close=frame['Close'].tail(50)))
+    r=50
+    fig.add_trace(go.Candlestick(x=frame['Date'].tail(r), open=frame['Open'].tail(r), high=frame['High'].tail(r), low=frame['Low'].tail(r), close=frame['Close'].tail(r)))
     #fig.add_trace(go.Scatter(x=frame['Date'], 
     #     y=frame['EMA20'], 
     #     opacity=0.7, 
