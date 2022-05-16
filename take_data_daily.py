103d8246a3024b0eb914f5983d6b444a80411d2a
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-05-16 08:43:16 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -359,7 +359,8 @@ for name, frame,framew in zip(names,framelist,framelistw):
                             sira +=1
                             expander()          
                 if option2 == 'Supertrend':
-                    if frame['Decision Super'].iloc[-1]=='Sell' or frame['Decision Super2'].iloc[-1]=='Sell' or frame['Decision Super3'].iloc[-1]=='Sell' :
+                    if frame['Decision Super'].iloc[-1]=='Sell' or frame['Decision Super2'].iloc[-1]=='Sell' or frame['Decision Super3'].iloc[-1]=='Sell'\
+                    and framew['Decision Super'].iloc[-1]=='Sell' or framew['Decision Super2'].iloc[-1]=='Sell' or framew['Decision Super3'].iloc[-1]=='Sell':
                             sira +=1
                             expander()   
                 #if option2 == 'Supertrend2':
