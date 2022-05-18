83baeff552b0a80be782bb8a28b5dcf73a4521ca
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-05-18 10:27:39 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -303,7 +303,7 @@ for name, frame,framew in zip(names,framelist,framelistw):
             if option1 == 'Buy' and framew['Trend MACD'].iloc[-1]=='Buy' and (framew['Dec_EMA50'].iloc[-1]=='Buy' or framew['MACD_diff'].iloc[-1]>0) : 
             #and framew['sup'].iloc[-1]==1 and framew['Dec_EMA50'].iloc[-1]=='Buy':
                 if option2 == 'EMA50':  
-                    if frame['EMA50_cross'].iloc[-1]=='Buy': #and frame['MACD_diff'].iloc[-1]>0:
+                    if frame['EMA50_cross'].iloc[-1]=='Buy' and frame['MACD_diff'].iloc[-1]>0 and frame['Trend MACD'].iloc[-1]=='Buy' :
                             sira +=1
                             expander()
                 if option2 == 'EMA200':  
@@ -328,8 +328,8 @@ for name, frame,framew in zip(names,framelist,framelistw):
                             sira +=1
                             expander()          
                 if option2 == 'Supertrend':
-                    if frame['Decision Super'].iloc[-1]=='Buy' or frame['Decision Super2'].iloc[-1]=='Buy' or frame['Decision Super3'].iloc[-1]=='Buy':
-                    #and frame['MACD_diff'].iloc[-1]>0:
+                    if frame['Decision Super'].iloc[-1]=='Buy' or frame['Decision Super2'].iloc[-1]=='Buy' or frame['Decision Super3'].iloc[-1]=='Buy'\
+                    and frame['MACD_diff'].iloc[-1]>0 and frame['Trend MACD'].iloc[-1]=='Buy' :
                             sira +=1
                             expander()
                # if option2 == 'Supertrend2':
