9e07a4782d7207e0d84ea14f97a667391d9a9aa0
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-05-20 08:44:13 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -130,17 +130,17 @@ def Supertrend(df):
     df.loc[(df.sup==1)&(df.sup.shift(1)==-1)|((df.Close.shift(1)>=df.sup2.shift(1))& \
     (df.Low<=df.sup2)&(df.Close>df.sup2)), 'Decision Super'] = 'Buy'
     df.loc[(df.sup==-1)&(df.sup.shift(1)==1)|((df.Close.shift(1)<=df.sup2.shift(1))& \
-    (df.Low>=df.sup2)&(df.Close<df.sup2)), 'Decision Super'] = 'Sell' 
+    (df.High>=df.sup2)&(df.Close<df.sup2)), 'Decision Super'] = 'Sell' 
     
     df.loc[(df.sup3==1)&(df.sup3.shift(1)==-1)|((df.Close.shift(1)>=df.sup4.shift(1))& \
     (df.Low<=df.sup4)&(df.Close>df.sup4)), 'Decision Super2'] = 'Buy'
     df.loc[(df.sup3==-1)&(df.sup3.shift(1)==1)|((df.Close.shift(1)<=df.sup4.shift(1))& \
-    (df.Low>=df.sup4)&(df.Close<df.sup4)), 'Decision Super2'] = 'Sell'   
+    (df.High>=df.sup4)&(df.Close<df.sup4)), 'Decision Super2'] = 'Sell'   
     
     df.loc[(df.sup5==1)&(df.sup5.shift(1)==-1)|((df.Close.shift(1)>=df.sup6.shift(1))& \
     (df.Low<=df.sup6)&(df.Close>df.sup6)), 'Decision Super3'] = 'Buy'
     df.loc[(df.sup5==-1)&(df.sup5.shift(1)==1)|((df.Close.shift(1)<=df.sup6.shift(1))& \
-    (df.Low>=df.sup6)&(df.Close<df.sup6)), 'Decision Super3'] = 'Sell' 
+    (df.High>=df.sup6)&(df.Close<df.sup6)), 'Decision Super3'] = 'Sell' 
     
     df.loc[(df.sup2 == df.sup2.shift(8))&(df.sup2 != df.sup2.shift(9)), 'Consolidating'] = 'Yes'
     df.loc[(df.sup4 == df.sup4.shift(8))&(df.sup4 != df.sup4.shift(9)), 'Consolidating2'] = 'Yes'
