978e9afb73668a8a566832b8c3adec16b6c2321c
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-06-01 12:26:01 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -353,7 +353,7 @@ for name, frame,framew in zip(names,framelist,framelistw):
             
             if option1 == 'Sell' and (framew['Dec_EMA50'].iloc[-1]=='Sell' or framew['Dec_EMA20'].iloc[-1]=='Sell'\
             and (framew['Stochrsi_k'].iloc[-1]<framew['Stochrsi_d'].iloc[-1]):
-                if option2 == 'EMASUPER':  
+                if option2 == 'EMASUPER':
                     if (frame['EMA50_cross'].iloc[-1]=='Sell' or frame['EMA20_cross'].iloc[-1]=='Sell' or frame['EMA200_cross'].iloc[-1]=='Sell'\
                     or frame['Decision Super'].iloc[-1]=='Sell' or frame['Decision Super2'].iloc[-1]=='Sell' or frame['Decision Super3'].iloc[-1]=='Sell')\
                     and (frame['Close'].iloc[-1]<frame['EMA20'].iloc[-1] and frame['MACD_diff'].iloc[-1]<0 and frame['Stochrsi_k'].iloc[-1]<frame['Stochrsi_d'].iloc[-1]):                            
