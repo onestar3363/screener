3dd5485be163041e815d270d7365df4755418284
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-06-28 08:40:49 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -314,10 +314,10 @@ st.header(option1 + option2)
 indices=['US500/USD_S&P 500_INDEX_US','EU50/EUR_Euro Stoxx 50_INDEX_DE','^N225','XU030.IS']
 for name, frame,framew in zip(names,framelist,framelistw): 
     try:
-        if  len(frame)>30 and len(framew)>30 and frame['ADX'].iloc[-1]>=adx_value and frame['RISK'].iloc[-1]<=riskvalue :
+        if  len(frame)>30 and len(framew)>30 and frame['ADX'].iloc[-1]>=adx_value and frame['RISK'].iloc[-1]<=riskvalue:
             
             if option1 == 'Buy' and (framew['Dec_EMA20'].iloc[-1]=='Buy' or framew['Dec_EMA50'].iloc[-1]=='Buy')\
-            and framew['Trend MACD'].iloc[-1]=='Buy':
+            and framew['Trend MACD'].iloc[-1]=='Buy' and (framew['Close'].iloc[-1]>framew['sup4'].iloc[-1] or framew['Close'].iloc[-1]>framew['sup6'].iloc[-1]):
             #framew['MACD_diff'].iloc[-1]>0 or framew['Stochrsi_k'].iloc[-1]>framew['Stochrsi_d'].iloc[-1] 
                 if option2 == 'EMASUPER':  
                     if (frame['Decision Super'].iloc[-1]=='Buy' or frame['Decision Super2'].iloc[-1]=='Buy' or frame['Decision Super3'].iloc[-1]=='Buy'\
