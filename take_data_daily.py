02b399b46618ffd9eabd6a6e7d8d9a062d2a0c40
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-06-28 09:13:39 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -335,7 +335,8 @@ for name, frame,framew in zip(names,framelist,framelistw):
                     if (frame['EMA50_cross'].iloc[-1]=='Sell2' or frame['EMA20_cross'].iloc[-1]=='Sell2' or frame['Decision Super'].iloc[-1]=='Sell'\
                     or frame['Decision Super2'].iloc[-1]=='Sell' or frame['Decision Super3'].iloc[-1]=='Sell')\
                     and (frame['Dec_EMA50'].iloc[-1]=='Sell' or frame['Dec_EMA20'].iloc[-1]=='Sell'\
-                    or frame['MACD_diff'].iloc[-1]<0 or frame['Trend MACD'].iloc[-1]=='Sell'):                    
+                    or frame['MACD_diff'].iloc[-1]<0 or frame['Trend MACD'].iloc[-1]=='Sell')\
+                    and (frame['Close'].iloc[-1]<frame['sup4'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup6'].iloc[-1]):
                             sira +=1
                             expander()
         if option2 == 'Index' and name in indices:
