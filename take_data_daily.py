5db1ae27c2b73af01498cc1aab6ef087e7118e59
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-06-28 08:53:25 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -322,17 +322,17 @@ for name, frame,framew in zip(names,framelist,framelistw):
                 if option2 == 'EMASUPER':  
                     if (frame['Decision Super'].iloc[-1]=='Buy' or frame['Decision Super2'].iloc[-1]=='Buy' or frame['Decision Super3'].iloc[-1]=='Buy'\
                     or frame['EMA50_cross'].iloc[-1]=='Buy' or frame['EMA20_cross'].iloc[-1]=='Buy')\
-                    and (frame['Dec_EMA50'].iloc[-1]=='Buy' or frame['Dec_EMA20'].iloc[-1]=='Buy')\
-                    and (frame['MACD_diff'].iloc[-1]>0 or frame['Trend MACD'].iloc[-1]=='Buy' ):
+                    and (frame['Dec_EMA50'].iloc[-1]=='Buy' or frame['Dec_EMA20'].iloc[-1]=='Buy'\
+                    or frame['MACD_diff'].iloc[-1]>0 or frame['Trend MACD'].iloc[-1]=='Buy' ):
                             sira +=1
                             expander()
             if option1 == 'Sell' and (framew['Dec_EMA20'].iloc[-1]=='Sell' or framew['Dec_EMA50'].iloc[-1]=='Sell')\
-            and framew['Trend MACD'].iloc[-1]=='Sell':   
+            and framew['Trend MACD'].iloc[-1]=='Sell' and (framew['Close'].iloc[-1]<framew['sup4'].iloc[-1] and framew['Close'].iloc[-1]<framew['sup6'].iloc[-1]):   
                 if option2 == 'EMASUPER':
                     if (frame['EMA50_cross'].iloc[-1]=='Sell2' or frame['EMA20_cross'].iloc[-1]=='Sell2' or frame['Decision Super'].iloc[-1]=='Sell'\
                     or frame['Decision Super2'].iloc[-1]=='Sell' or frame['Decision Super3'].iloc[-1]=='Sell')\
-                    and (frame['Dec_EMA50'].iloc[-1]=='Sell')\
-                    and (frame['MACD_diff'].iloc[-1]<0): #or frame['Trend MACD'].iloc[-1]=='Sell'):                    
+                    and (frame['Dec_EMA50'].iloc[-1]=='Sell' or frame['Dec_EMA20'].iloc[-1]=='Sell'\
+                    or frame['MACD_diff'].iloc[-1]<0) or frame['Trend MACD'].iloc[-1]=='Sell'):                    
                             sira +=1
                             expander()
         if option2 == 'Index' and name in indices:
