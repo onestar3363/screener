7a0ce6b8cc71a99ceff45e962ca78f749dd57437
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-07-04 11:48:39 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -336,13 +336,10 @@ for name, frame,framew in zip(names,framelist,framelistw):
         if  len(frame)>30 and len(framew)>30 and frame['ADX'].iloc[-1]>=adx_value and frame['RISK'].iloc[-1]<=riskvalue:
             
             if option1 == 'Buy' and (framew['Dec_EMA20'].iloc[-1]=='Buy' or framew['Dec_EMA50'].iloc[-1]=='Buy')\
-             and (framew['Close'].iloc[-1]>framew['sup2'].iloc[-1])\
-             and (framew['Close'].iloc[-1]>framew['sup4'].iloc[-1] or framew['Close'].iloc[-1]>framew['sup6'].iloc[-1]):
-             #and framew['Trend MACD'].iloc[-1]=='Buy'
-             #framew['MACD_diff'].iloc[-1]>0 
+             and (framew['Close'].iloc[-1]>framew['sup2'].iloc[-1]) or framew['Close'].iloc[-1]>framew['sup4'].iloc[-1] or framew['Close'].iloc[-1]>framew['sup6'].iloc[-1]):
                 if option2 == 'EMASUPER':  
-                    if (frame['Decision Super'].iloc[-1]=='Buy3' or frame['Decision Super2'].iloc[-1]=='Buy3' or frame['Decision Super3'].iloc[-1]=='Buy3'\
-                    or frame['EMA50_cross'].iloc[-1]=='Buy3' or frame['EMA20_cross'].iloc[-1]=='Buy3')\
+                    if (frame['Decision Super'].iloc[-1]=='Buy2' or frame['Decision Super2'].iloc[-1]=='Buy2' or frame['Decision Super3'].iloc[-1]=='Buy2'\
+                    or frame['EMA50_cross'].iloc[-1]=='Buy2' or frame['EMA20_cross'].iloc[-1]=='Buy2')\
                     or (frame['Decision Super'].iloc[-1]=='Buy2' or frame['Decision Super2'].iloc[-1]=='Buy2' or frame['Decision Super3'].iloc[-1]=='Buy2'\
                     or frame['EMA50_cross'].iloc[-1]=='Buy2' or frame['EMA20_cross'].iloc[-1]=='Buy2')\
                     and (frame['Dec_EMA50'].iloc[-1]=='Buy' or frame['Dec_EMA20'].iloc[-1]=='Buy'):
