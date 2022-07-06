55f44ca384bb8660b1c4331c032d94dcf1107ece
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-07-06 13:26:57 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -336,11 +336,11 @@ for name, frame,framew in zip(names,framelist,framelistw):
             and (framew['Close'].iloc[-1]<framew['sup2'].iloc[-1])\
             and (framew['Close'].iloc[-1]<framew['sup4'].iloc[-1] or framew['Close'].iloc[-1]<framew['sup6'].iloc[-1]):
                 if option2 == 'EMASUPER':
-                   if frame['Decision Super2'].iloc[-1]=='Sell' or frame['Decision Super3'].iloc[-1]=='Sell'\
+                   if frame['Decision Super3'].iloc[-1]=='Sell'\
                     or frame['EMA50_cross'].iloc[-1]=='Sell'\
-                    or frame['Decision Super2'].iloc[-1]=='Sell2' or frame['Decision Super3'].iloc[-1]=='Sell2'\
+                    or frame['Decision Super3'].iloc[-1]=='Sell2'\
                     or frame['EMA50_cross'].iloc[-1]=='Sell2'\
-                    or frame['Decision Super2'].iloc[-1]=='Sell3' or frame['Decision Super3'].iloc[-1]=='Sell3'\
+                    or frame['Decision Super3'].iloc[-1]=='Sell3'\
                     or frame['EMA50_cross'].iloc[-1]=='Sell3' :
                             sira +=1
                             expander()
