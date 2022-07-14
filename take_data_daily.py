3b2972131f3c960a4adf6d67c87697d55b6b1cf7
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-07-14 16:23:06 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -338,11 +338,11 @@ for name, frame,framew in zip(names,framelist,framelistw):
             #and (framew['Close'].iloc[-1]<framew['sup4'].iloc[-1] or framew['Close'].iloc[-1]<framew['sup6'].iloc[-1]):
                 if option2 == 'EMASUPER':
                    if (frame['EMA50_cross'].iloc[-1]=='Sell' or frame['EMA50_cross'].iloc[-1]=='Sell3'\
-                   or frame['EMA20_cross'].iloc[-1]=='Sell3'\
-                   or frame['Decision Super2'].iloc[-1]=='Sell' or frame['Decision Super2'].iloc[-1]=='Sell3'\
-                   or frame['Decision Super'].iloc[-1]=='Sell'\
-                   or frame['Decision Super3'].iloc[-1]=='Sell' or frame['Decision Super3'].iloc[-1]=='Sell2' or frame['Decision Super3'].iloc[-1]=='Sell3')\
-                   and (frame['Close'].iloc[-1]<frame['sup6'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup4'].iloc[-1]):
+                   or frame['EMA20_cross'].iloc[-1]=='Sell3')\
+                   #or frame['Decision Super2'].iloc[-1]=='Sell' or frame['Decision Super2'].iloc[-1]=='Sell3'\
+                   #or frame['Decision Super'].iloc[-1]=='Sell'\
+                   #or frame['Decision Super3'].iloc[-1]=='Sell' or frame['Decision Super3'].iloc[-1]=='Sell2' or frame['Decision Super3'].iloc[-1]=='Sell3')\
+                   and (frame['Close'].iloc[-1]<frame['sup6'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup4'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup2'].iloc[-1]):
                   
                             sira +=1
                             expander()
