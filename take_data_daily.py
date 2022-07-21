92899311f5b2c1365fff28d4035376101366c7e7
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-07-22 00:08:08 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -302,9 +302,9 @@ def expander():
         col3.write(frame[['Close','EMA20_cross','EMA50_cross','Decision Super','Decision Super2','Decision Super3','Dec_MACD','ADX','Trend MACD','MACD_diff']].tail(2))
         col4.write(framew[['Close','ATR%','ADX','Dec_EMA50','Dec_MACD','Trend MACD','MACD_diff']].tail(2))
         col1, col2 = st.columns([1, 1])
-        r=100
+        r=200
         fig=get_figures(frame,r)
-        r=20
+        r=40
         figw=get_figures(framew,r)
         col1.plotly_chart(fig,use_container_width=True)
         col2.plotly_chart(figw,use_container_width=True)        
