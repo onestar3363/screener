3da8772ff04dc5cb72eaab98d23e0bf108a96ef0
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-07-27 14:59:55 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -127,27 +127,23 @@ def Supertrend(df):
     
     
     df.loc[(df.sup==1)&(df.sup.shift(1)==-1), 'Decision Super'] = 'Buy'
-    #df.loc[(df.Close.shift(1)>=df.sup2.shift(1))&(df.Low<=df.sup2)&(df.Close>df.sup2), 'Decision Super'] = 'Buy2'
     df.loc[(df.Close.shift(1)<=df.sup2.shift(1))&(df.High>=df.sup2)&(df.Close<df.sup2)|\
     ((df.Close.shift(1)>=df.sup2.shift(1))&(df.Low<=df.sup2)&(df.Close>df.sup2)), 'Decision Super'] = 'SB'
     df.loc[(df.sup==-1)&(df.sup.shift(1)==1), 'Decision Super'] = 'Sell' 
-    #df.loc[(df.Close.shift(1)<=df.sup2.shift(1))&(df.High>=df.sup2), 'Decision Super'] = 'Sell2'
-    df.loc[(df.Close.shift(1)>=df.sup2.shift(1))&(df.Low<=df.sup2)&(df.Close>df.sup2), 'Decision Super'] = 'SBL'
+
     
     
     df.loc[(df.sup3==1)&(df.sup3.shift(1)==-1), 'Decision Super2'] = 'Buy'
-    #df.loc[(df.Close.shift(1)>=df.sup4.shift(1))&(df.Low<=df.sup4)&(df.Close>df.sup4), 'Decision Super2'] = 'Buy2'
-    df.loc[(df.Close.shift(1)<=df.sup4.shift(1))&(df.High>=df.sup4)&(df.Close<df.sup4), 'Decision Super2'] = 'SBH'
+    df.loc[(df.Close.shift(1)<=df.sup4.shift(1))&(df.High>=df.sup4)&(df.Close<df.sup4)|\
+    ((df.Close.shift(1)>=df.sup4.shift(1))&(df.Low<=df.sup4)&(df.Close>df.sup4)), 'Decision Super2'] = 'SB'
     df.loc[(df.sup3==-1)&(df.sup3.shift(1)==1), 'Decision Super2'] = 'Sell'
-    #df.loc[(df.Close.shift(1)<=df.sup4.shift(1))&(df.High>=df.sup4)&(df.Close<df.sup4), 'Decision Super2'] = 'Sell2'
-    df.loc[(df.Close.shift(1)>=df.sup4.shift(1))&(df.Low<=df.sup4)&(df.Close>df.sup4), 'Decision Super2'] = 'SBL'
+
     
     df.loc[(df.sup5==1)&(df.sup5.shift(1)==-1), 'Decision Super3'] = 'Buy'
-    #df.loc[(df.Close.shift(1)>=df.sup6.shift(1))&(df.Low<=df.sup6)&(df.Close>df.sup6), 'Decision Super3'] = 'Buy2'
-    df.loc[(df.Close.shift(1)<=df.sup6.shift(1))&(df.High>=df.sup6)&(df.Close<df.sup6), 'Decision Super3'] = 'SBH'
+    df.loc[(df.Close.shift(1)<=df.sup6.shift(1))&(df.High>=df.sup6)&(df.Close<df.sup6)|\
+    ((df.Close.shift(1)>=df.sup6.shift(1))&(df.Low<=df.sup6)&(df.Close>df.sup6)), 'Decision Super3'] = 'SB'
     df.loc[(df.sup5==-1)&(df.sup5.shift(1)==1), 'Decision Super3'] = 'Sell' 
-    #df.loc[(df.Close.shift(1)<=df.sup6.shift(1))&(df.High>=df.sup6)&(df.Close<df.sup6), 'Decision Super3'] = 'Sell2' 
-    df.loc[(df.Close.shift(1)>=df.sup6.shift(1))&(df.Low<=df.sup6)&(df.Close>df.sup6), 'Decision Super3'] = 'SBL'
+
     
     df.loc[(df.sup2 == df.sup2.shift(2)), 'Consolidating'] = 'Yes'
     df.loc[(df.sup4 == df.sup4.shift(2)), 'Consolidating2'] = 'Yes'
@@ -346,8 +342,8 @@ for name, frame,framew in zip(names,framelist,framelistw):
                    if (frame['EMA50_cross'].iloc[-1]=='Sell'\
                    #or frame['Decision Super'].iloc[-1]=='Sell'\
                    #(frame['Decision Super'].iloc[-1]=='Sell3' and frame['Consolidating'].iloc[-1]=='Yes')\
-                   or frame['Decision Super2'].iloc[-1]=='Sell' or frame['Decision Super2'].iloc[-1]=='Sell3'\
-                   or frame['Decision Super3'].iloc[-1]=='Sell' or frame['Decision Super3'].iloc[-1]=='Sell3')\
+                   or frame['Decision Super2'].iloc[-1]=='Sell' or frame['Decision Super2'].iloc[-1]=='SB'\
+                   or frame['Decision Super3'].iloc[-1]=='Sell' or frame['Decision Super3'].iloc[-1]=='SB')\
                    and frame['Close'].iloc[-1]<frame['sup6'].iloc[-1]\
                    and (frame['Dec_EMA20'].iloc[-1]=='Sell' or frame['Dec_EMA50'].iloc[-1]=='Sell'):
                    #and frame['EMA50'].iloc[-1]<(1+(fark/100))*frame['EMA20'].iloc[-1]
