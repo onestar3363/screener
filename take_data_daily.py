4b24a32ca686ced05f7d389773cfb990a61ee942
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-07-29 08:16:13 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -331,18 +331,18 @@ for name, frame,framew in zip(names,framelist,framelistw):
                     and (frame['Dec_EMA20'].iloc[-1]=='Buy' or frame['Dec_EMA50'].iloc[-1]=='Buy'):
                             sira +=1
                             expander()
-            if option1 == 'Sell' and (framew['Dec_EMA20'].iloc[-1]=='Sell' or framew['Dec_EMA50'].iloc[-1]=='Sell'):
-            #or framew['Close'].iloc[-1]<framew['sup2'].iloc[-1]):
+            if option1 == 'Sell' and (framew['Dec_EMA20'].iloc[-1]=='Sell' or framew['Dec_EMA50'].iloc[-1]=='Sell'\
+            or framew['Close'].iloc[-1]<framew['sup2'].iloc[-1]):
             #and (framew['Dec_EMA20'].iloc[-1]=='Sell' or framew['Dec_EMA50'].iloc[-1]=='Sell')
             #and (framew['Close'].iloc[-1]<framew['sup4'].iloc[-1] or framew['Close'].iloc[-1]<framew['sup6'].iloc[-1])
                 if option2 == 'EMASUPER':
                    if (frame['EMA50_cross'].iloc[-1]=='Sell'\
                    #or frame['Decision Super'].iloc[-1]=='Sell'\
                    #(frame['Decision Super'].iloc[-1]=='Sell3' and frame['Consolidating'].iloc[-1]=='Yes')\
-                   or frame['Decision Super2'].iloc[-1]=='Sell' or frame['Decision Super2'].iloc[-1]=='SB'\
-                   or frame['Decision Super3'].iloc[-1]=='Sell' or frame['Decision Super3'].iloc[-1]=='SB')\
+                   or frame['Decision Super2'].iloc[-1]=='Sell'\
+                   or frame['Decision Super3'].iloc[-1]=='Sell')\
                    and (frame['Dec_EMA20'].iloc[-1]=='Sell' or frame['Dec_EMA50'].iloc[-1]=='Sell')\
-                   and (frame['Close'].iloc[-1]<frame['sup6'].iloc[-1] and frame['Close'].iloc[-1]<frame['sup4'].iloc[-1]):
+                   and (frame['Close'].iloc[-1]<frame['sup6'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup4'].iloc[-1]):
                    #and frame['EMA50'].iloc[-1]<(1+(fark/100))*frame['EMA20'].iloc[-1]
                    #and (frame['Close'].iloc[-1]<frame['sup2'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup4'].iloc[-1]\
                             sira +=1
