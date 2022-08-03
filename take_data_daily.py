3bc4b07afb83040b1019ab41df4ffd2125d4484d
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-08-03 08:20:40 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -318,9 +318,9 @@ for name, frame,framew in zip(names,framelist,framelistw):
     try:
         if  len(frame)>30 and len(framew)>30 and frame['ADX'].iloc[-1]>=adx_value and frame['RISK'].iloc[-1]<=riskvalue:
             
-            if option1 == 'Buy' and (framew['Trend MACD'].iloc[-1]=='Buy'\
-            or framew['Dec_EMA20'].iloc[-1]=='Buy' or framew['Dec_EMA50'].iloc[-1]=='Buy'\
-            or framew['Close'].iloc[-1]>framew['sup2'].iloc[-1]):
+            if option1 == 'Buy' and framew['Trend MACD'].iloc[-1]=='Buy':
+            #or framew['Dec_EMA20'].iloc[-1]=='Buy' or framew['Dec_EMA50'].iloc[-1]=='Buy'\
+            #or framew['Close'].iloc[-1]>framew['sup2'].iloc[-1]):
                 if option2 == 'EMASUPER':
                     if (frame['Decision Super'].iloc[-1]=='Buy' or frame['Decision Super2'].iloc[-1]=='Buy' or frame['Decision Super3'].iloc[-1]=='Buy'\
                     or frame['EMA50_cross'].iloc[-1]=='Buy'\
@@ -348,8 +348,9 @@ for name, frame,framew in zip(names,framelist,framelistw):
                     #and (frame['Close'].iloc[-1]<frame['sup6'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup4'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup4'].iloc[-1])
                             sira +=1
                             expander()                            
-            if option1 == 'Sell' and (framew['Dec_EMA20'].iloc[-1]=='Sell' or framew['Dec_EMA50'].iloc[-1]=='Sell'\
-            or framew['Close'].iloc[-1]<framew['sup2'].iloc[-1] or framew['Trend MACD'].iloc[-1]=='Sell'):
+            if option1 == 'Sell' and framew['Trend MACD'].iloc[-1]=='Sell':
+            #(framew['Dec_EMA20'].iloc[-1]=='Sell' or framew['Dec_EMA50'].iloc[-1]=='Sell'\
+            #or framew['Close'].iloc[-1]<framew['sup2'].iloc[-1] or
             #and (framew['Dec_EMA20'].iloc[-1]=='Sell' or framew['Dec_EMA50'].iloc[-1]=='Sell')
             #and (framew['Close'].iloc[-1]<framew['sup4'].iloc[-1] or framew['Close'].iloc[-1]<framew['sup6'].iloc[-1])
                 if option2 == 'EMASUPER':
