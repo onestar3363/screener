8df6342d2b700ec3a239778f0f9452db4d2971c0
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-08-03 08:11:05 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -321,9 +321,6 @@ for name, frame,framew in zip(names,framelist,framelistw):
             if option1 == 'Buy' and (framew['Trend MACD'].iloc[-1]=='Buy'\
             or framew['Dec_EMA20'].iloc[-1]=='Buy' or framew['Dec_EMA50'].iloc[-1]=='Buy'\
             or framew['Close'].iloc[-1]>framew['sup2'].iloc[-1]):
-            #and (framew['Dec_EMA20'].iloc[-1]=='Buy' or framew['Dec_EMA50'].iloc[-1]=='Buy'):
-            #and (framew['Close'].iloc[-1]>framew['sup2'].iloc[-1])\
-            #and (framew['Close'].iloc[-1]>framew['sup4'].iloc[-1] or framew['Close'].iloc[-1]>framew['sup6'].iloc[-1]):
                 if option2 == 'EMASUPER':
                     if (frame['Decision Super'].iloc[-1]=='Buy' or frame['Decision Super2'].iloc[-1]=='Buy' or frame['Decision Super3'].iloc[-1]=='Buy'\
                     or frame['EMA50_cross'].iloc[-1]=='Buy'\
@@ -352,7 +349,7 @@ for name, frame,framew in zip(names,framelist,framelistw):
                             sira +=1
                             expander()                            
             if option1 == 'Sell' and (framew['Dec_EMA20'].iloc[-1]=='Sell' or framew['Dec_EMA50'].iloc[-1]=='Sell'\
-            or framew['Close'].iloc[-1]<framew['sup2'].iloc[-1]) and framew['Trend MACD'].iloc[-1]=='Sell':
+            or framew['Close'].iloc[-1]<framew['sup2'].iloc[-1] or framew['Trend MACD'].iloc[-1]=='Sell'):
             #and (framew['Dec_EMA20'].iloc[-1]=='Sell' or framew['Dec_EMA50'].iloc[-1]=='Sell')
             #and (framew['Close'].iloc[-1]<framew['sup4'].iloc[-1] or framew['Close'].iloc[-1]<framew['sup6'].iloc[-1])
                 if option2 == 'EMASUPER':
