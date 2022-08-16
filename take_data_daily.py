7f8130b201de23267e39283c7a5d31dfaddeb572
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-08-16 21:06:44 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -343,7 +343,7 @@ for name, frame,framew in zip(names,framelist,framelistw):
                 if option2 == 'Consolidating':
                     if (frame['Consolidating'].iloc[-1]=='Yes' and frame['Consolidating2'].iloc[-1]=='Yes' and frame['Consolidating3'].iloc[-1]=='Yes')\
                     and (frame['EMA20'].iloc[-1]>frame['EMA50'].iloc[-1] or frame['Dec_EMA50'].iloc[-1]=='Buy')\
-                    and (frame['Close'].iloc[-1]>frame['Close'].iloc[-2] or frame['Close'].iloc[-1]>frame['Open'].iloc[-1])\
+                    and (frame['Close'].iloc[-1]<frame['Close'].iloc[-2] or frame['Close'].iloc[-1]<frame['Open'].iloc[-1])\
                     and (frame['Close'].iloc[-1]>frame['sup4'].iloc[-1] or frame['Close'].iloc[-1]>frame['sup6'].iloc[-1]):
                     #and (frame['Dec_EMA20'].iloc[-1]=='Buy' and frame['Dec_EMA50'].iloc[-1]=='Sell'):
                     #and (frame['Close'].iloc[-1]<frame['sup6'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup4'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup4'].iloc[-1])
