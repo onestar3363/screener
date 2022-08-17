cd7f3c7094774374b6e2a5519a200a09542014e1
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-08-17 23:32:31 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -354,10 +354,9 @@ for name, frame,framew in zip(names,framelist,framelistw):
             #and (framew['Dec_EMA20'].iloc[-1]=='Sell' or framew['Dec_EMA50'].iloc[-1]=='Sell')
             #and (framew['Close'].iloc[-1]<framew['sup4'].iloc[-1] or framew['Close'].iloc[-1]<framew['sup6'].iloc[-1])
                 if option2 == 'EMASUPER':
-                   if (frame['Decision Super2'].iloc[-1]=='Sell'\
-                   or (frame['Decision Super'].iloc[-1]=='Sell' and frame['Consolidating'].iloc[-2]=='Yes'))\
-                   and frame['EMA20'].iloc[-1]>frame['EMA50'].iloc[-1]\
-                   and (frame['Dec_EMA50'].iloc[-1]=='Sell' or frame['Dec_EMA20'].iloc[-1]=='Sell'):
+                   if (frame['Decision Super2'].iloc[-1]=='Sell' or frame['EMA50_cross'].iloc[-1]=='Sell'\
+                   or (frame['Decision Super'].iloc[-1]=='Sell' and frame['Dec_EMA50'].iloc[-1]=='Sell')):
+                   #and (frame['Dec_EMA50'].iloc[-1]=='Sell' or frame['Dec_EMA20'].iloc[-1]=='Sell'):
                    #and (frame['Close'].iloc[-1]<frame['sup4'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup6'].iloc[-1]):
                    #and frame['EMA50'].iloc[-1]<(1+(fark/100))*frame['EMA20'].iloc[-1]
                    #and (frame['Close'].iloc[-1]<frame['sup2'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup4'].iloc[-1]\
