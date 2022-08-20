cbaff0a3926668b6e718f207895334ba632570d3
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-08-21 02:47:01 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -378,7 +378,7 @@ for name, frame,framew in zip(names,framelist,framelistw):
                    if (frame['Decision Super2'].iloc[-1]=='Sell' or frame['Decision Super3'].iloc[-1]=='Sell'\
                    or frame['Decision Super2'].iloc[-1]=='Sell2' or frame['Decision Super3'].iloc[-1]=='Sell2'\
                    or frame['EMA50_cross'].iloc[-1]=='Sell' or frame['EMA50_cross'].iloc[-1]=='Sell2')\
-                   and (frame['Close'].iloc[-1]<frame['sup4'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup6'].iloc[-1]):
+                   and (frame['Close'].iloc[-1]<frame['sup4'].iloc[-1] and frame['Close'].iloc[-1]<frame['sup6'].iloc[-1]):
                    #or (frame['Decision Super'].iloc[-1]=='Sell' and frame['Dec_EMA50'].iloc[-1]=='Sell')):
                    #and (frame['Dec_EMA50'].iloc[-1]=='Sell' or frame['Dec_EMA20'].iloc[-1]=='Sell'):
                    #and (frame['Close'].iloc[-1]<frame['sup4'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup6'].iloc[-1]):
