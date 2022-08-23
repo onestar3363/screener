0f4600c078aa85320bbc68ce78a858c4e4e5176d
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-08-23 03:04:05 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -362,7 +362,7 @@ for name, frame,framew in zip(names,framelist,framelistw):
 
                 if option2 == 'Consolidating':
                     if (frame['Consolidating'].iloc[-1]=='Yes' and frame['Consolidating2'].iloc[-1]=='Yes' and frame['Consolidating3'].iloc[-1]=='Yes')\
-                    and (frame['EMA20'].iloc[-1]>frame['EMA50'].iloc[-1])\
+                    and (frame['EMA20'].iloc[-1]>frame['EMA50'].iloc[-1] or frame['Dec_EMA50'].iloc[-1]=='Buy')\
                     and (frame['Close'].iloc[-1]>frame['sup2'].iloc[-1]>frame['sup4'].iloc[-1]>frame['sup6'].iloc[-1]):
                     #and (frame['Dec_EMA20'].iloc[-1]=='Buy' and frame['Dec_EMA50'].iloc[-1]=='Sell'):
                     #and (frame['Close'].iloc[-1]<frame['sup6'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup4'].iloc[-1] or frame['Close'].iloc[-1]<frame['sup4'].iloc[-1])
@@ -401,7 +401,7 @@ for name, frame,framew in zip(names,framelist,framelistw):
                 if option2 == 'Consolidating':
                     if (frame['Consolidating'].iloc[-1]=='Yes' and frame['Consolidating2'].iloc[-1]=='Yes' and frame['Consolidating3'].iloc[-1]=='Yes')\
                     and (frame['EMA20'].iloc[-1]<frame['EMA50'].iloc[-1])\
-                    and (frame['Close'].iloc[-1]<frame['sup2'].iloc[-1]<frame['sup4'].iloc[-1]<frame['sup6'].iloc[-1]):          
+                    and (frame['Close'].iloc[-1]<frame['sup4'].iloc[-1]<frame['sup6'].iloc[-1]):          
                             sira +=1
                             expander()                       
         if option2 == 'Index' and name in indices:
