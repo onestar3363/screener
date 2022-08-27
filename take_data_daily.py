c1c38be813171c9c55416717ab9b2dc4db10d534
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-08-27 17:05:17 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -321,7 +321,7 @@ def expander(cond):
            figw=get_figures(framew,r)
            col1.plotly_chart(fig,use_container_width=True)
            col2.plotly_chart(figw,use_container_width=True)      
-     return cond
+           return cond
 sira=0
 option1 = st.sidebar.selectbox("Buy or Sell",('Buy','Sell')) 
 option2 = st.sidebar.selectbox("Which Indicator?", ('EMASUPER','Consolidating','Increasing','Index','EMA50','Supertrend','EMA20','MACD','ADX','EMA200'))
