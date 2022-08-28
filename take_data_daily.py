4efb6872dcf7c6c99057b4602fb6a4da4b1e4023
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-08-28 21:18:54 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -309,7 +309,7 @@ def get_figures(frame,r):
     #fig.update_layout( height=600, width=1200,
 
 def expander(cond):
-       with st.expander(cond+str(sira) +') '+ name+'/'+' RISK= '+str(frame['RISK'].iloc[-1].round(2))+'/ %ATR='+str(frame['ATR%'].iloc[-1].round(2))):
+       with st.expander(cond+ str(sira) +') '+ name+'/'+' RISK= '+str(frame['RISK'].iloc[-1].round(2))+'/ %ATR='+str(frame['ATR%'].iloc[-1].round(2))):
            #st.write(str(sira) +') '+ name+'/'+' RISK= '+str(frame['RISK'].iloc[-1].round(2))+'/ %ATR='+str(frame['ATR%'].iloc[-1].round(2)))
            col3, col4 = st.columns([1, 1])
            col3.write(frame[['Close','ADX','EMA20_cross','EMA50_cross','Decision Super','Decision Super2','Decision Super3','Dec_MACD','Trend MACD','MACD_diff']].tail(2))
