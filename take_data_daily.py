6a22067f8f9f86e6a71baaa372c2215ba2f798e2
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-08-29 16:16:09 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -343,7 +343,7 @@ for name, frame,framew in zip(names,framelist,framelistw):
                 if option2 == 'EMASUPER':
                     if (frame['Decision Super2'].iloc[-h]=='Buy'\
                     or frame['EMA50_cross'].iloc[-h]=='Buy')\
-                    and frame['Dec_EMA50'].iloc[-h]=='Buy'\
+                    and frame['Dec_EMA50'].iloc[-h]=='Buy':
                     #and (frame['Close'].iloc[-h]>frame['sup6'].iloc[-h] or frame['Close'].iloc[-h]>frame['sup4'].iloc[-h]):
                     #and frame['EMA20'].iloc[-h]<frame['EMA50'].iloc[-h]\
                             sira +=1
@@ -359,13 +359,13 @@ for name, frame,framew in zip(names,framelist,framelistw):
                 if option2 == 'EMASUPER':
                    if (frame['Decision Super2'].iloc[-h]=='Sell' or frame['EMA50_cross'].iloc[-h]=='Sell'\
                    or frame['Decision Super'].iloc[-h]=='Sell')\
-                   and (frame['Close'].iloc[-h]<frame['sup4'].iloc[-h] or frame['Close'].iloc[-h]<frame['sup6'].iloc[-h])\
+                   #and (frame['Close'].iloc[-h]<frame['sup4'].iloc[-h] or frame['Close'].iloc[-h]<frame['sup6'].iloc[-h])\
                    and frame['Dec_EMA50'].iloc[-h]=='Sell':
                             sira +=1
                             expander('emasuper')
                    if (frame['Consolidating2'].iloc[-1]=='Yes' and frame['Consolidating3'].iloc[-1]=='Yes')\
                     and frame['Dec_EMA50'].iloc[-1]=='Sell'\
-                    #and frame['Close'].iloc[-1]<frame['sup2'].iloc[-h]<frame['sup4'].iloc[-h]<frame['sup6'].iloc[-h]:
+                    and frame['Close'].iloc[-1]<frame['sup2'].iloc[-h]<frame['sup4'].iloc[-h]<frame['sup6'].iloc[-h]:
                     #and frame['Decision ADX'].iloc[-1]=='Buy'
                             sira +=1
                             expander('consolidating')
