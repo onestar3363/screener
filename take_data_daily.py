91f10ee7f9ec567ae9e11fb7ec06a7abc9463fa2
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-08-29 11:26:37 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -341,14 +341,14 @@ for name, frame,framew in zip(names,framelist,framelistw):
             #or framew['Trend MACD'].iloc[-1]=='Buy'
            
                 if option2 == 'EMASUPER':
-                    if (frame['Decision Super2'].iloc[-h]=='Buy'\
-                    or frame['EMA50_cross'].iloc[-h]=='Buy')\
-                    and frame['Dec_EMA50'].iloc[-h]=='Buy'\
-                    and (frame['Close'].iloc[-h]>frame['sup6'].iloc[-h] or frame['Close'].iloc[-h]>frame['sup4'].iloc[-h]):
-                    #and frame['EMA20'].iloc[-h]<frame['EMA50'].iloc[-h]\
-                            sira +=1
-                            expander('emasuper')
-                    elif (frame['Consolidating2'].iloc[-1]=='Yes' and frame['Consolidating3'].iloc[-1]=='Yes')\
+                    #if (frame['Decision Super2'].iloc[-h]=='Buy'\
+                    #or frame['EMA50_cross'].iloc[-h]=='Buy')\
+                    #and frame['Dec_EMA50'].iloc[-h]=='Buy'\
+                    #and (frame['Close'].iloc[-h]>frame['sup6'].iloc[-h] or frame['Close'].iloc[-h]>frame['sup4'].iloc[-h]):
+                    ##and frame['EMA20'].iloc[-h]<frame['EMA50'].iloc[-h]\
+                    #        sira +=1
+                    #        expander('emasuper')
+                    if (frame['Consolidating2'].iloc[-1]=='Yes' and frame['Consolidating3'].iloc[-1]=='Yes')\
                     and frame['Decision ADX'].iloc[-1]=='Buy'\
                     and frame['Dec_EMA50'].iloc[-1]=='Buy':
                     #and frame['Close'].iloc[-1]>frame['sup2'].iloc[-1]>frame['sup4'].iloc[-h]>frame['sup6'].iloc[-h]
@@ -363,10 +363,10 @@ for name, frame,framew in zip(names,framelist,framelistw):
                    and frame['Dec_EMA50'].iloc[-h]=='Sell':
                             sira +=1
                             expander('emasuper')
-                   elif (frame['Consolidating2'].iloc[-1]=='Yes' and frame['Consolidating3'].iloc[-1]=='Yes')\
-                    and frame['Close'].iloc[-1]<frame['sup4'].iloc[-h]<frame['sup6'].iloc[-h]\
-                    and frame['Decision ADX'].iloc[-1]=='Buy':
-                    #and frame['Dec_EMA50'].iloc[-1]=='Sell':
+                   elif (frame['Consolidating2'].iloc[-1]=='Yes' and frame['Consolidating3'].iloc[-1]=='Yes')\                    
+                    and frame['Decision ADX'].iloc[-1]=='Buy'\                    
+                    and frame['Dec_EMA50'].iloc[-1]=='Sell':
+                    #and frame['Close'].iloc[-1]<frame['sup4'].iloc[-h]<frame['sup6'].iloc[-h]\
                             sira +=1
                             expander('consolidating')
         if option2 == 'Index' and name in indices:
