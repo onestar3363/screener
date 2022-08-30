5f3e4672b3f7ab097fe338e4a3005008fad85804
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-08-30 23:56:16 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -349,16 +349,16 @@ for name, frame,framew in zip(names,framelist,framelistw):
                     #        sira +=1
                     #        expander('emasuper')
                     if (frame['Consolidating'].iloc[-h]=='Yes' and frame['Consolidating2'].iloc[-h]=='Yes' and frame['Consolidating3'].iloc[-h]=='Yes')\
-                    and frame['Close'].iloc[-h]>frame['sup2'].iloc[-h]>frame['sup4'].iloc[-h]>frame['sup6'].iloc[-h]:
-                    and frame['Decision ADX'].iloc[-h]=='Buy'
+                     and frame['Close'].iloc[-h]>frame['sup2'].iloc[-h]>frame['sup4'].iloc[-h]>frame['sup6'].iloc[-h]:
+                     and frame['Decision ADX'].iloc[-h]=='Buy'
                     #and frame['Dec_EMA50'].iloc[-h]=='Buy'\
                             sira +=1
                             expander('consolidating')
                     if (frame['Consolidating'].iloc[-(h+1)]=='Yes' and frame['Consolidating2'].iloc[-(h+1)]=='Yes' and frame['Consolidating3'].iloc[-(h+1)]=='Yes')\
-                    and frame['sup2'].iloc[-h]>frame['sup2'].iloc[-(h+1)]\
-                    and frame['Close'].iloc[-(h+1)]>frame['sup2'].iloc[-(h+1)]>frame['sup4'].iloc[-(h+1)]>frame['sup6'].iloc[-(h+1)]\
-                    and frame['Close'].iloc[-h]>frame['sup2'].iloc[-h]>frame['sup4'].iloc[-h]>frame['sup6'].iloc[-h]\
-                    and frame['Decision ADX'].iloc[-h]=='Buy':
+                     and frame['sup2'].iloc[-h]>frame['sup2'].iloc[-(h+1)]\
+                     and frame['Close'].iloc[-(h+1)]>frame['sup2'].iloc[-(h+1)]>frame['sup4'].iloc[-(h+1)]>frame['sup6'].iloc[-(h+1)]\
+                     and frame['Close'].iloc[-h]>frame['sup2'].iloc[-h]>frame['sup4'].iloc[-h]>frame['sup6'].iloc[-h]\
+                     and frame['Decision ADX'].iloc[-h]=='Buy':
                             sira +=1
                             expander('atak')
             if option1 == 'Sell' and (framew['Close'].iloc[-1]<framew['sup2'].iloc[-1] or framew['Dec_EMA50'].iloc[-h]=='Sell'):
