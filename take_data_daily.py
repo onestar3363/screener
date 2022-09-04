e24738e1e88128570d2bf0dab55df329f44e21a8
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-09-04 23:01:04 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -355,13 +355,13 @@ for name, frame,framew in zip(names,framelist,framelistw):
                     #and frame['Dec_EMA50'].iloc[-h]=='Buy'\
                             sira +=1
                             expander('consolidating')
-                    #if (frame['Consolidating'].iloc[-(h+1)]=='Yes' and frame['Consolidating2'].iloc[-(h+1)]=='Yes' and frame['Consolidating3'].iloc[-(h+1)]=='Yes')\
-                    # and frame['sup2'].iloc[-h]>frame['sup2'].iloc[-(h+1)]\
-                    # and frame['Close'].iloc[-(h+1)]>frame['sup2'].iloc[-(h+1)]>frame['sup4'].iloc[-(h+1)]>frame['sup6'].iloc[-(h+1)]\
-                    # and frame['Close'].iloc[-h]>frame['sup2'].iloc[-h]>frame['sup4'].iloc[-h]>frame['sup6'].iloc[-h]\
-                    # and frame['Decision ADX'].iloc[-h]=='Buy':
-                    #        sira +=1
-                    #        expander('atak')
+                    if (frame['Consolidating'].iloc[-(h+1)]=='Yes' and frame['Consolidating2'].iloc[-(h+1)]=='Yes' and frame['Consolidating3'].iloc[-(h+1)]=='Yes')\
+                     and frame['sup2'].iloc[-h]>frame['sup2'].iloc[-(h+1)]\
+                     and frame['Close'].iloc[-(h+1)]>frame['sup2'].iloc[-(h+1)]>frame['sup4'].iloc[-(h+1)]>frame['sup6'].iloc[-(h+1)]\
+                     and frame['Close'].iloc[-h]>frame['sup2'].iloc[-h]>frame['sup4'].iloc[-h]>frame['sup6'].iloc[-h]\
+                     and frame['Decision ADX'].iloc[-h]=='Buy':
+                            sira +=1
+                            expander('atak')
                     #if frame['Close'].iloc[-h]>frame['sup2'].iloc[-h]>frame['sup4'].iloc[-h]>frame['sup6'].iloc[-h]\
                     # and frame['Decision ADX'].iloc[-h]=='Buy'\
                     # and frame['sup2'].iloc[-h]>frame['sup2'].iloc[-(h+1)]:
