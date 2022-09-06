346efba66710d059247933fd00d46096ee465e19
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-09-06 23:37:48 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -349,7 +349,7 @@ for name, frame,framew in zip(names,framelist,framelistw):
                     #        sira +=1
                     #        expander('emasuper')
                     if (frame['Consolidating'].iloc[-h]=='Yes' and frame['Consolidating2'].iloc[-h]=='Yes' and frame['Consolidating3'].iloc[-h]=='Yes')\
-                     and (frame['Dec_MACD'].iloc[-1]=='Buy'):
+                     and (frame['Trend MACD'].iloc[-1]=='Buy'):
                      #and frame['ADX'].iloc[-1]<frame['ADX'].iloc[-2]\   
                      #and frame['Close'].iloc[-h]>frame['sup2'].iloc[-h]>frame['sup4'].iloc[-h]>frame['sup6'].iloc[-h]\
                      #and frame['Decision ADX'].iloc[-h]=='Buy':
@@ -384,7 +384,7 @@ for name, frame,framew in zip(names,framelist,framelistw):
                 #            sira +=1
                 #            expander('emasuper')
                    if (frame['Consolidating'].iloc[-h]=='Yes' and frame['Consolidating2'].iloc[-h]=='Yes' and frame['Consolidating3'].iloc[-h]=='Yes')\
-                    and (frame['Dec_MACD'].iloc[-h]=='Sell' or frame['Trend MACD'].iloc[-1]=='Sell'):
+                    and (frame['Trend MACD'].iloc[-1]=='Sell'):
                     #and frame['ADX'].iloc[-1]<frame['ADX'].iloc[-2]\
                     #and frame['Close'].iloc[-h]<frame['sup2'].iloc[-h]<frame['sup4'].iloc[-h]<frame['sup6'].iloc[-h]\
                     #and frame['Decision ADX'].iloc[-h]=='Buy':
