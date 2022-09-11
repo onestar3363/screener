ee0edac08dbb0919ba44cf1ea1b154dcb57f8cd1
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-09-12 00:17:54 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -362,10 +362,10 @@ for name, frame,framew in zip(names,framelist,framelistw):
                             sira +=1
                             expander('pullback')                          
                     elif (frame['Consolidating'].iloc[-h1]=='Yes' or frame['Consolidating2'].iloc[-h1]=='Yes' or frame['Consolidating3'].iloc[-h1]=='Yes')\
-                     and frame['Close'].iloc[-1]>frame['sup2'].iloc[-1]\
-                     and frame['Close'].iloc[-1]>frame['EMA200'].iloc[-1]\
+                     and frame['Close'].iloc[-h1]>frame['sup2'].iloc[-h1]\
+                     and frame['Close'].iloc[-h1]>frame['EMA200'].iloc[-h1]\
                      and frame['Dec_EMA50'].iloc[-h1]=='Buy'\
-                     and frame['Dec_MACD'].iloc[-1]=='Buy':
+                     and frame['Dec_MACD'].iloc[-h1]=='Buy':
                      #and frame['ADX'].iloc[-1]<frame['ADX'].iloc[-2]\   
                      #and frame['Close'].iloc[-h]>frame['sup2'].iloc[-h]>frame['sup4'].iloc[-h]>frame['sup6'].iloc[-h]\
                      #and frame['Decision ADX'].iloc[-h]=='Buy':                    
