ccdbdf361af7e3dc5d2103188c5092e4691c8027
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-10-24 22:41:45 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -352,13 +352,12 @@ for name, frame,framew in zip(names,framelist,framelistw):
                     and frame['Dec_MACD'].iloc[-1]=='Buy':
                             sira +=1
                             expander('breakout')                          
-                   #if (frame['Decision Super2'].iloc[-h1]=='Buy2' or frame['Decision Super3'].iloc[-h1]=='Buy2'\
-                   # or frame['EMA50_cross'].iloc[-h1]=='Buy2' or frame['EMA20_cross'].iloc[-h1]=='Buy2')\
-                   # and frame['Dec_EMA200'].iloc[-h1]=='Buy':
-                   # #and (frame['Close'].iloc[-1]>frame['sup6'].iloc[-h] or frame['Close'].iloc[-1]>frame['sup4'].iloc[-1]):
-                   # #and frame['EMA20'].iloc[-h]<frame['EMA50'].iloc[-h]\
-                   #         sira +=1
-                   #         expander('pullback')                           
+                   if (frame['Decision Super2'].iloc[-h1]=='Buy2' or frame['Decision Super3'].iloc[-h1]=='Buy2'\
+                   or frame['EMA50_cross'].iloc[-h1]=='Buy2' or frame['EMA20_cross'].iloc[-h1]=='Buy2')\
+                   and frame['Dec_EMA200'].iloc[-h1]=='Buy'\
+                   and (frame['Close'].iloc[-1]>frame['sup6'].iloc[-h] or frame['Close'].iloc[-1]>frame['sup4'].iloc[-1]):
+                            sira +=1
+                            expander('pullback')                           
                    #if (frame['Consolidating2'].iloc[-h1]=='Yes' and frame['Consolidating3'].iloc[-h1]=='Yes')\
                    # and (frame['Dec_EMA50'].iloc[-h1]=='Buy' or frame['Dec_EMA20'].iloc[-h1]=='Buy')\
                    # and frame['Dec_EMA200'].iloc[-h1]=='Buy'\
