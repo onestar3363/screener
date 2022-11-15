0d90fece644d846c120ffca50569ddb34e7613cb
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-11-15 14:10:37 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -402,12 +402,12 @@ for name, frame,framew in zip(names,framelist,framelistw):
                    #and (frame['Close'].iloc[-h]<frame['sup4'].iloc[-h] or frame['Close'].iloc[-h]<frame['sup6'].iloc[-h])\
                             sira +=1
                             expander('breakout')
-                   elif (framew['Decision Super3'].iloc[-2]=='Sell' or framew['Decision Super'].iloc[-2]=='Sell'):
+                   elif (framew['Decision Super'].iloc[-2]=='Sell' or framew['Decision Super3'].iloc[-2]=='Sell'):
                    #and (framew['Dec_EMA50'].iloc[-2]=='Sell' or framew['Dec_EMA20'].iloc[-2]=='Sell') :
                    #and (framew['Close'].iloc[-2]<framew['sup4'].iloc[-2] or framew['Close'].iloc[-2]<framew['sup6'].iloc[-2])\
                             sira +=1
                             expander('weak breakout')
-                   elif (framew['Decision Super2'].iloc[-2]=='Sell2' or framew['Decision Super3'].iloc[-2]=='Sell2')\
+                   elif (framew['Decision Super'].iloc[-2]=='Sell2' or framew['Decision Super3'].iloc[-2]=='Sell2')\
                    and (framew['Dec_EMA50'].iloc[-2]=='Sell' or framew['Dec_EMA20'].iloc[-2]=='Sell'):
                    #and (framew['Close'].iloc[-h]<framew['sup4'].iloc[-h] or framew['Close'].iloc[-h]<framew['sup6'].iloc[-h])
                             sira +=1
