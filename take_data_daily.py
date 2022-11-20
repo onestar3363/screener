45730e41d80638a0e047c7746b3043d9e2e26a85
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-11-20 22:14:39 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -347,7 +347,8 @@ for name, frame,framew in zip(names,framelist,framelistw):
             #or framew['Close'].iloc[-1]>framew['sup2'].iloc[-1] or framew['Close'].iloc[-1]>framew['sup4'].iloc[-1])):
            
                 if option2 == 'EMASUPER':
-                   if (frame['Decision Super2'].iloc[-h1]=='Buy' or frame['Decision Super3'].iloc[-h1]=='Buy')\
+                   if (frame['Decision Super2'].iloc[-h1]=='Buy' or frame['Decision Super3'].iloc[-h1]=='Buy'\
+                   or frame['EMA50_cross'].iloc[-h1]=='Buy' or frame['EMA20_cross'].iloc[-h1]=='Buy')\
                    and (frame['Dec_EMA50'].iloc[-h1]=='Buy' or frame['Dec_EMA20'].iloc[-h1]=='Buy'):
                    #and frame['Dec_MACD'].iloc[-1]=='Buy':
                    #and (frame['Dec_EMA200'].iloc[-h1]=='Buy')
@@ -361,15 +362,15 @@ for name, frame,framew in zip(names,framelist,framelistw):
                    #and (frame['Dec_EMA200'].iloc[-h1]=='Buy')
                    #and (frame['Close'].iloc[-h1]>frame['sup6'].iloc[-h1] or frame['Close'].iloc[-h1]>frame['sup4'].iloc[-h1])\
                             sira +=1
-                            expander('weak breakout')                    
-                   #elif (framew['EMA50_cross'].iloc[-2]=='Buy2' or framew['EMA20_cross'].iloc[-2]=='Buy2')\
-                   #and (framew['Dec_EMA50'].iloc[-2]=='Buy' or framew['Dec_EMA20'].iloc[-2]=='Buy'):
-                   ##or framew['EMA50_cross'].iloc[-2]=='Buy2' or framew['EMA20_cross'].iloc[-2]=='Buy2')\
-                   ##and framew['Dec_EMA200'].iloc[-h1]=='Buy':
-                   ##and frame['Dec_MACD'].iloc[-h1]=='Buy':
-                   ##and (frame['Close'].iloc[-1]>frame['sup6'].iloc[-h] or frame['Close'].iloc[-1]>frame['sup4'].iloc[-1]):
-                   #         sira +=1
-                   #         expander('week pullback')                             
+                            expander('week breakout')                    
+                   elif (framew['Decision Super2'].iloc[-2]=='Buy2' or framew['Decision Super3'].iloc[-2]=='Buy2'\
+                   or framew['EMA50_cross'].iloc[-2]=='Buy2' or framew['EMA20_cross'].iloc[-2]=='Buy2')\
+                   and (framew['Dec_EMA50'].iloc[-2]=='Buy' or framew['Dec_EMA20'].iloc[-2]=='Buy')\
+                   and framew['Close'].iloc[-1]>framew['Close'].iloc[-2]:
+                   #and frame['Dec_MACD'].iloc[-h1]=='Buy':
+                   #and (frame['Close'].iloc[-1]>frame['sup6'].iloc[-h] or frame['Close'].iloc[-1]>frame['sup4'].iloc[-1]):
+                            sira +=1
+                            expander('week pullback')                             
                    elif (frame['Decision Super2'].iloc[-h1-1]=='Buy2' or frame['Decision Super3'].iloc[-h1-1]=='Buy2'\
                    or frame['EMA50_cross'].iloc[-h1-1]=='Buy2' or frame['EMA20_cross'].iloc[-h1-1]=='Buy2')\
                    and (frame['Dec_EMA50'].iloc[-h1-1]=='Buy' or frame['Dec_EMA20'].iloc[-h1-1]=='Buy')\
