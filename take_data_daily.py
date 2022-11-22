db251f9e4183eb39d9cb3c561255c4453eda38a5
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-11-22 23:34:59 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -416,10 +416,10 @@ for name, frame,framew in zip(names,framelist,framelistw):
                    #and (framew['Dec_EMA50'].iloc[-2]=='Sell' or framew['Dec_EMA20'].iloc[-2]=='Sell')\
                             sira +=1
                             expander('week pullback')                     
-                   elif (frame['Decision Super2'].iloc[-h1-1]=='Sell2' or frame['Decision Super3'].iloc[-h1-1]=='Sell2'\
-                   or frame['EMA50_cross'].iloc[-h1-1]=='Sell2')\
-                   and (frame['Dec_EMA50'].iloc[-h1-1]=='Sell' or frame['Dec_EMA20'].iloc[-h1-1]=='Sell' or frame['Dec_EMA200'].iloc[-h1-1]=='Sell' )\
-                   and frame['Close'].iloc[-2]>frame['Close'].iloc[-1]:
+                   elif (frame['Decision Super2'].iloc[-h1]=='Sell2' or frame['Decision Super3'].iloc[-h1]=='Sell2'\
+                   or frame['EMA50_cross'].iloc[-h1]=='Sell2')\
+                   and (frame['Dec_EMA50'].iloc[-h1]=='Sell' or frame['Dec_EMA20'].iloc[-h1]=='Sell' or frame['Dec_EMA200'].iloc[-h1]=='Sell' ):
+                   #and frame['Close'].iloc[-2]>frame['Close'].iloc[-1]:
                    #and frame['Dec_MACD'].iloc[-h1]=='Buy':
                    #and (frame['Close'].iloc[-1]>frame['sup6'].iloc[-h] or frame['Close'].iloc[-1]>frame['sup4'].iloc[-1]):
                             sira +=1
