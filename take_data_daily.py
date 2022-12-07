c8e74e18b53988273b626a3c114472a8f11e6997
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-12-07 12:10:24 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -381,11 +381,11 @@ for name, frame,framew in zip(names,framelist,framelistw):
                    #and (frame['Close'].iloc[-1]>frame['sup6'].iloc[-h] or frame['Close'].iloc[-1]>frame['sup4'].iloc[-1]):
                             sira +=1
                             expander('pullback')
-                   elif (frame['Consolidating2'].iloc[-h1]=='Yes' or frame['Consolidating3'].iloc[-h1]=='Yes')\
-                   and (frame['Dec_EMA50'].iloc[-h1]=='Buy'and frame['Dec_EMA20'].iloc[-h1]=='Buy')\
-                   and frame['Close'].iloc[-h1]/frame['EMA50'].iloc[-h1]<1+(3*frame['ATR%'].iloc[-h1]/100)\
-                   and frame['Dec_EMA200'].iloc[-h1]=='Buy'\
-                   and frame['Close'].iloc[-h1]>frame['sup2'].iloc[-h1]:
+                   #elif (frame['Consolidating2'].iloc[-h1]=='Yes' or frame['Consolidating3'].iloc[-h1]=='Yes')\
+                   #and (frame['Dec_EMA50'].iloc[-h1]=='Buy'and frame['Dec_EMA20'].iloc[-h1]=='Buy')\
+                   #and frame['Close'].iloc[-h1]/frame['EMA50'].iloc[-h1]<1+(3*frame['ATR%'].iloc[-h1]/100)\
+                   #and frame['Dec_EMA200'].iloc[-h1]=='Buy'\
+                   #and frame['Close'].iloc[-h1]>frame['sup2'].iloc[-h1]:
                    #and frame['Dec_MACD'].iloc[-h1]=='Buy'\
                    #and frame['Close'].iloc[-h1]>frame['Close'].iloc[-h1-1]\
                    #and (frame['Close'].iloc[-h1]>frame['sup2'].iloc[-h1] >frame['sup4'].iloc[-h1] >frame['sup6'].iloc[-h1])\
@@ -393,8 +393,8 @@ for name, frame,framew in zip(names,framelist,framelistw):
                    #and frame['ADX'].iloc[-h1]>frame['ADX'].iloc[-(h1+1)]:
                    #and frame['Close'].iloc[-h]>frame['sup2'].iloc[-h]>frame['sup4'].iloc[-h]>frame['sup6'].iloc[-h]\
                    #and frame['Decision ADX'].iloc[-h]=='Buy':                    
-                            sira +=1
-                            expander('consolidating')                             
+                            #sira +=1
+                            #expander('consolidating')                             
                    #elif (frame['Close'].iloc[-h1]>frame['sup2'].iloc[-h1]>frame['sup4'].iloc[-h1] >frame['sup6'].iloc[-h1])\
                    #and (frame['Dec_EMA50'].iloc[-h1]=='Buy' or frame['Dec_EMA20'].iloc[-h1]=='Buy')\
                    #and frame['Close'].iloc[-h1]>frame['Close'].iloc[-h1-1]\
