4a6e2290741f2db6b4775f59dbf303303e4812fe
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-12-16 12:21:17 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -453,8 +453,8 @@ for name, frame,framew in zip(names,framelist,framelistw):
                             sira +=1
                             expander('pullback')
                    elif (frame['Consolidating2'].iloc[-h1]=='Yes' and frame['Consolidating3'].iloc[-h1]=='Yes')\
-                   and frame['Close'].iloc[-h1]>frame['sup2'].iloc[-h1]>frame['sup4'].iloc[-h1]>frame['sup6'].iloc[-h1]\
-                   and frame['Dec_MACD'].iloc[-1]=='Sell':
+                   and frame['Close'].iloc[-h1]>frame['sup4'].iloc[-h1]>frame['sup6'].iloc[-h1]\
+                   and frame['Dec_MACD'].iloc[-h1]=='Sell':
                    #and frame['ADX'].iloc[-1]<frame['ADX'].iloc[-2]\
                    #and frame['Decision ADX'].iloc[-h]=='Buy':
                    #and frame['Dec_EMA50'].iloc[-1]=='Sell'
