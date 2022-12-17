65e824c4793be5105ca1f30b0d78ad7885584638
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-12-17 15:26:51 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -456,7 +456,7 @@ for name, frame,framew in zip(names,framelist,framelistw):
                             expander('pullback')
                 if option2=='consolidating':
                    if (frame['Consolidating2'].iloc[-h1]=='Yes' and frame['Consolidating3'].iloc[-h1]=='Yes')\
-                   and frame['Dec_MACD'].iloc[-h1]=='Sell'\                 
+                   and frame['Dec_MACD'].iloc[-h1]=='Sell'\
                    and frame['Close'].iloc[-h1]>frame['sup2'].iloc[-h1]>frame['sup4'].iloc[-h1]>frame['sup6'].iloc[-h1]:
                    #and frame['Dec_EMA20'].iloc[-1]=='Sell'\
                    #and frame['ADX'].iloc[-1]<frame['ADX'].iloc[-2]\
